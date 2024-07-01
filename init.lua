15820f274392438ce4edb2f849e667b3f1cdd2cd
Josh Owen <josh.owen@universalfieldrobots.com.au>
2024-07-01 11:55:55 +1000

    More clipboard fixes

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus'
 
 vim.g.clipboard = {
   name = 'OSC 52',
